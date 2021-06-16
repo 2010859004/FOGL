c1d4cb61b3a6374ca76e22b262cf8650accd79e1
Andreas Dötzl <a.doetzl@tac.eu.com>
2021-06-17 00:34:17 +0200

    Refactoring

--- at/fhburgenland/fogl/statemachine/uebung3/MathFormulaStateMachine.java
+++ at/fhburgenland/fogl/statemachine/uebung3/MathFormulaStateMachine.java
@@ -41,92 +41,10 @@ class MathFormulaStateMachine implements StateMachine {
 
       final Alphabet alphabet = optionalAlphabet.get();
 
-      if (currentState == State.S_START) {
-        if (alphabet.isNumber()) {
-          currentState = State.A;
-        } else if (alphabet == Alphabet.INPUT_OPENING_BRACKET_SIGN) {
-          currentState = State.C;
-        } else {
-          return createResultGenerator(ResultConstant.INVALID);
-        }
+      currentState = currentState.checkState(alphabet);
+      if (currentState == State.INVALID) {
+        return createResultGenerator(ResultConstant.INVALID);
       }
-
-      else if (currentState == State.A) {
-        if (alphabet.isNumber()) {
-          continue;
-        } else if (alphabet.isMathSign()) {
-          currentState = State.S_START;
-        } else if (alphabet == Alphabet.INPUT_EQUALS_SIGN) {
-          currentState = State.B;
-        } else {
-          return createResultGenerator(ResultConstant.INVALID);
-        }
-      }
-
-      else if (currentState == State.B) {
-        if (!alphabet.isNumber()) {
-          return createResultGenerator(ResultConstant.INVALID);
-        } else {
-          currentState = State.Z_END;
-        }
-      }
-
-      else if (currentState == State.Z_END) {
-        if (alphabet.isNumber()) {
-          continue;
-        } else {
-          return createResultGenerator(ResultConstant.INVALID);
-        }
-      }
-
-      else if (currentState == State.C) {
-        if (alphabet.isNumber()) {
-          currentState = State.D;
-        } else {
-          return createResultGenerator(ResultConstant.INVALID);
-        }
-      }
-
-      else if (currentState == State.D) {
-        if (alphabet.isNumber()) {
-          continue;
-        } else if (alphabet == Alphabet.INPUT_CLOSING_BRACKET_SIGN) {
-          currentState = State.G;
-        } else if (alphabet.isMathSign()) {
-          currentState = State.E;
-        } else {
-          return createResultGenerator(ResultConstant.INVALID);
-        }
-      }
-
-      else if (currentState == State.E) {
-        if (alphabet.isNumber()) {
-          currentState = State.F;
-        } else {
-          return createResultGenerator(ResultConstant.INVALID);
-        }
-      }
-
-      else if (currentState == State.F) {
-        if (alphabet.isNumber()) {
-          continue;
-        } else if (alphabet == Alphabet.INPUT_CLOSING_BRACKET_SIGN) {
-          currentState = State.G;
-        } else {
-          return createResultGenerator(ResultConstant.INVALID);
-        }
-      }
-
-      else if (currentState == State.G) {
-        if (alphabet == Alphabet.INPUT_EQUALS_SIGN) {
-          currentState = State.B;
-        } else if (alphabet.isMathSign()) {
-          currentState = State.S_START;
-        } else {
-          return createResultGenerator(ResultConstant.INVALID);
-        }
-      }
-
     }
 
     return createResultGenerator(currentState == State.Z_END ? ResultConstant.VALID : ResultConstant.INVALID);
--- at/fhburgenland/fogl/statemachine/uebung3/State.java
+++ at/fhburgenland/fogl/statemachine/uebung3/State.java
@@ -6,14 +6,119 @@ package at.fhburgenland.fogl.statemachine.uebung3;
  * Date: 15.05.2021
  * Time: 12:28
  */
-enum State {
-  S_START,
-  A,
-  B,
-  C,
-  D,
-  E,
-  F,
-  G,
-  Z_END,
+enum State implements Check {
+  S_START {
+    @Override
+    public State checkState(Alphabet alphabet) {
+      if (alphabet.isNumber()) {
+        return State.A;
+      } else if (alphabet == Alphabet.INPUT_OPENING_BRACKET_SIGN) {
+        return State.C;
+      } else {
+        return INVALID;
+      }
+    }
+  },
+  A {
+    @Override
+    public State checkState(Alphabet alphabet) {
+      if (alphabet.isNumber()) {
+        return this;
+      } else if (alphabet.isMathSign()) {
+        return State.S_START;
+      } else if (alphabet == Alphabet.INPUT_EQUALS_SIGN) {
+        return State.B;
+      } else {
+        return INVALID;
+      }
+    }
+  },
+  B {
+    @Override
+    public State checkState(Alphabet alphabet) {
+      if (!alphabet.isNumber()) {
+        return INVALID;
+      } else {
+        return State.Z_END;
+      }
+    }
+  },
+  C {
+    @Override
+    public State checkState(Alphabet alphabet) {
+      if (alphabet.isNumber()) {
+        return State.D;
+      } else {
+        return INVALID;
+      }
+    }
+  },
+  D {
+    @Override
+    public State checkState(Alphabet alphabet) {
+      if (alphabet.isNumber()) {
+        return this;
+      } else if (alphabet == Alphabet.INPUT_CLOSING_BRACKET_SIGN) {
+        return State.G;
+      } else if (alphabet.isMathSign()) {
+        return State.E;
+      } else {
+        return INVALID;
+      }
+    }
+  },
+  E {
+    @Override
+    public State checkState(Alphabet alphabet) {
+      if (alphabet.isNumber()) {
+        return State.F;
+      } else {
+        return INVALID;
+      }
+    }
+  },
+  F {
+    @Override
+    public State checkState(Alphabet alphabet) {
+      if (alphabet.isNumber()) {
+        return this;
+      } else if (alphabet == Alphabet.INPUT_CLOSING_BRACKET_SIGN) {
+        return State.G;
+      } else {
+        return INVALID;
+      }
+    }
+  },
+  G {
+    @Override
+    public State checkState(Alphabet alphabet) {
+      if (alphabet == Alphabet.INPUT_EQUALS_SIGN) {
+        return State.B;
+      } else if (alphabet.isMathSign()) {
+        return State.S_START;
+      } else {
+        return INVALID;
+      }
+    }
+  },
+  Z_END {
+    @Override
+    public State checkState(Alphabet alphabet) {
+      if (alphabet.isNumber()) {
+        return this;
+      } else {
+        return INVALID;
+      }
+    }
+  },
+  INVALID;
+
+  @Override
+  public State checkState(Alphabet alphabet) {
+    return INVALID;
+  }
+}
+
+interface Check {
+  State checkState(Alphabet alphabet);
 }
