7bb8eaf0a59ccf2f34a346ee47dbfc117b36d0ab
Andreas Dötzl <a.doetzl@tac.eu.com>
2021-06-21 09:14:05 +0200

    DB Test Änderungen

--- at/fhburgenland/fogl/statemachine/uebung3/Uebung3AppStarter.java
+++ at/fhburgenland/fogl/statemachine/uebung3/Uebung3AppStarter.java
@@ -19,7 +19,7 @@ public class Uebung3AppStarter {
     final File file = new File(System.getProperty("user.dir") + separator +
         filePath
     );
-    final DBConfiguration dbConfiguration = new DBConfiguration("jdbc:postgresql://10.11.12.13:5432/fogl-db", "bswe", "fogl", "SELECT * FROM Formeln");
+    final DBConfiguration dbConfiguration = new DBConfiguration("jdbc:postgresql://10.11.12.13:5432/fogl-dbhue", "bswe", "fogl", "SELECT * FROM Formeln");
     new CommandLineInterface(StateMachineCreator::createMathFormulaStateMachine, file, "Bitte geben Sie eine Formel ein:", dbConfiguration)
         .startMenu();
   }
